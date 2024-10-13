d60121bb294ccd4c588afdcaaeee4561bfd49fd9
LegendWandellion22 <legendwand@hotmail.com>
2024-10-13 01:21:24 +0000

    Ahh just trying this out little bit

---
+++ wandelldesigns/ExampleUsage.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Chapter9
+{
+    public class ExampleUsage : MonoBehaviour
+    {
+        public GameObject prefabToPool;
+        public int poolSize = 10;
+        public string objectName = "MyTag";
+        //This is just something I've imported for the f of it all!
+        public ChracterController myController;
+        
+
+        void Start()
+        {
+            // Create an object pool with the specified prefab, pool size, and tag 
+            ObjectPoolManager.Instance.CreateObjectPool(prefabToPool, poolSize, objectName);
+
+            // Get an object from the pool 
+            GameObject obj = ObjectPoolManager.Instance.GetPooledObject(objectName);
+
+            if (obj != null)
+            {
+                // Use the object 
+                obj.transform.position = Vector3.zero;
+            }
+
+            // Return the object to the pool 
+            ObjectPoolManager.Instance.ReturnToPool(objectName, obj);
+        }
+
+    }
+}
